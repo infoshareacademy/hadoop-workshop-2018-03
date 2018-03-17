3d3fcb8d005c3b3b1dc04fe7dead61e1219e1efa
Jakub Podeszwik <jakubpodeszwik@gmail.com>
2018-03-17 20:37:19 +0100

    udf instruction

--- code/hive-udfs/src/main/java/pl/isa/hadoop/HelloWorldUdf.java
+++ code/hive-udfs/src/main/java/pl/isa/hadoop/HelloWorldUdf.java
@@ -6,6 +6,6 @@ import org.apache.hadoop.hive.ql.exec.UDF;
 @Description(value = "_FUNC_ returns 'hello ' + input")
 public class HelloWorldUdf extends UDF {
     public String evaluate(String input) {
-        return "hello2 " + input;
+        return "hello " + input;
     }
 }
